60cd5e91ae0ff070f08583adfb8120de82c6f065
dynamic-distribution-2838 <harshrpatel93@gmail.com>
2019-03-08 18:45:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   new_array = []
-  array.each do |i|
-    num = array[i]
-    new_array << num**2
+  array.each do |num|
+    new_array << (num * num)
   end
   new_array
 end
